57a434f9c3d8d9408d41c0fef19d449331ebba80
Aisha Yusuff <a.yusuff99@outlook.com>
2022-11-10 20:15:33 +0000

    Add tests for multiplication and divide

--- src/main/java/com/codingblackfemales/Calculator.java
+++ src/main/java/com/codingblackfemales/Calculator.java
@@ -2,18 +2,22 @@ package com.codingblackfemales;
 
 public class Calculator {
     public static Integer add(final int firstInput, final int secondInput) {
+        
         return firstInput + secondInput;
     }
 
     public static Integer subtract(final int firstInput, final int secondInput) {
+        
         return firstInput - secondInput;
     }
 
     public static Integer multiply(final int firstInput, final int secondInput) {
+        
         return firstInput * secondInput;
     }
 
-    public static Integer divide(final int firstInput, final int secondInput) {
+    public static Float divide(final float firstInput, final float secondInput) {
+        
         return firstInput / secondInput;
     }
 }
